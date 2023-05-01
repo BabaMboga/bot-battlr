da333876728947ccb77817dca53793db29f0c774
AyimWilliam <odhisayim@gmail.com>
2023-05-02 00:40:40 +0300

    App component formatted

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,51 @@
-import React, {useState, useEffect} from 'react'
-import BotCollection from './BotCollection'
-import YourBotArmy from './YourBotArmy'
+import React, { useState, useEffect } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 function App() {
-    const [bots, setBots] = useState([]);
-    const [army, setArmy] = useState([]);
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
 
-    useEffect (() => {
-        fetch('http://localhost:3000/bots')
-            .then(response => response.json())
-            .then(bots => setBots(bots));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then((response) => response.json())
+      .then((bots) => setBots(bots));
+  }, []);
 
-    const addBotToArmy = bot => {
-        if (!army.includes(bot)){
-            setArmy([...army, bot]);
-        }
-    };
+  const addBotToArmy = (bot) => {
+    if (!army.includes(bot)) {
+      setArmy([...army, bot]);
+    }
+  };
 
-    const removeBotArmy = bot => {
-        setArmy(army.filter(b => b !== bot));
-    };
+  const removeBotArmy = (bot) => {
+    setArmy(army.filter((b) => b !== bot));
+  };
 
-    const dischargeBot = bot => {
-        fetch(`http://localhost:3000/bots/${bot.id}`, {
-            method: 'DELETE',
-        })
-            .then(() => {
-                setBots(bots.filter(b => b !== bot));
-                setArmy(army.filter(b => b !== bot));
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
+  const dischargeBot = (bot) => {
+    fetch(`http://localhost:3000/bots/${bot.id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setBots(bots.filter((b) => b !== bot));
+        setArmy(army.filter((b) => b !== bot));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="App">
-        <h1>Bot Army</h1>
-        <BotCollection bots={bots} addBotToArmy={addBotToArmy} />
-        <YourBotArmy army={army} removedBotFromArmy={removeBotArmy} dischargeBot={dischargeBot} />
+      <h1>Bot Army</h1>
+      <BotCollection bots={bots} addBotToArmy={addBotToArmy} />
+      <YourBotArmy
+        army={army}
+        removedBotFromArmy={removeBotArmy}
+        dischargeBot={dischargeBot}
+      />
     </div>
   );
 }
 
-export default App
+export default App;
