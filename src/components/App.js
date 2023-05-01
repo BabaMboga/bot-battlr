f57a66e1a470e3b110e14a58eefacaca925a767b
AyimWilliam <odhisayim@gmail.com>
2023-05-02 00:08:00 +0300

    useEffect for fetch implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from 'react'
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 function App() {
+    const [bots, setBots] = useState([]);
+    const [army, setArmy] = useState([]);
+
+    useEffect (() => {
+        fetch('http://localhost:3000/bots')
+            .then(response => response.json())
+            .then(bots => setBots(bots));
+    }, []);
+    
   return (
     <div>App</div>
   )
