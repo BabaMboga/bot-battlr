34440359331356199a9098f714019585042b37c4
AyimWilliam <odhisayim@gmail.com>
2023-05-02 00:44:44 +0300

    Apply botcard component to botcollection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -5,4 +5,15 @@ function BotCollection({bots, addBotToArmy}){
     const handleClick = bot => {
         addBotToArmy(bot);
     };
+
+    return (
+        <div className="bot-collection">
+            {bots.map(bot => (
+                <BotCard key={bot.id} bot={bot} handleClick={handleClick} buttonText="Enlist" />
+            ))}
+
+        </div>
+    );
 }
+
+export default BotCollection;
